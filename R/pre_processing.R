a9ae2d9f35bbceac497874eb45b66b461caa6f37
JaredChung <jared_chung@hotmail.com>
2017-11-20 21:59:29 +1100

    Create template for preprocessing script

---
+++ R/pre_processing.R
@@ -0,0 +1,14 @@
+
+# ========================================== #
+# Project automatic forecast ML
+# Jared Chung
+# 23/08/2017
+# file: Time_Series_preprocessing
+# ========================================== #
+
+#' Forecast using standard forecasting
+#'
+#' @param data Time series object as an input
+#' @param
+#'
+#' @example
