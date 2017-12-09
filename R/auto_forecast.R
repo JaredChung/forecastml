aeaf49b03d972fdc15c9a56911c067cb7708e1ab
JaredChung <jared_chung@hotmail.com>
2017-12-09 21:56:04 +1100

    remove train test split and cross validation

--- R/auto_forecast.R
+++ R/auto_forecast.R
@@ -22,46 +22,8 @@ library(tidyverse)
 library(zoo)
 library(lubridate)
 library(parallel)
-source(feature_extracter)
+source("R/utils.R")
 #library(R6) convert to R6 Class later ....
-
-
-# Standard 80/20 train test split
-train_test_data <- function(data) {
-
-      if("ts" %in% class(data)) {
-        data <- data.frame(date = as.Date(yearmon(time(data))), value=  as.matrix(data))
-        train = data[1:floor(nrow(data)*0.8),]
-        test = data[(floor(nrow(data)*0.8)+1):nrow(data),]
-      } else {
-        train = data[1:floor(nrow(data)*0.8),]
-        test = data[(floor(nrow(data)*0.8)+1):nrow(data),]
-      }
-      return(list(train=train,test=test))
-
-}
-
-
-# Cross validation
-cross_validation_data <- function(data,
-                                  initialwindow = 0.7,
-                                  horizon = 12,
-                                  fixedWindow = TRUE) {
-
-      if ("ts" %in% class(data)) {
-          return(timeslice <- caret::createTimeSlices(1:length(data),
-                                               initialWindow = length(data) * initialwindow,
-                                               horizon = horizon,
-                                               fixedWindow = fixedWindow))
-      } else {
-          return(timeslice <- caret::createTimeSlices(1:nrow(data),
-                                               initialWindow = nrow(data) * initialwindow,
-                                               horizon = horizon,
-                                               fixedWindow = fixedWindow))
-      }
-      return(timeslice)
-}
-
 
 # Access forecast package
 run_forecast <- function(train, test,FUN, name, timeslice,  train_regressor= NULL, test_regressor=NULL,...) {
