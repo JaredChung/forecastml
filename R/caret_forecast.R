851f2f823e2f0b4705cfdbfbb363f74fe52788c6
JaredChung <jared_chung@hotmail.com>
2018-02-15 16:54:35 +1100

    start caret file

---
+++ R/caret_forecast.R
@@ -0,0 +1,31 @@
+# ========================================== #
+# Project automatic forecast ML
+# Jared Chung
+# 23/08/2017
+# file: machine_learning_forecast
+# ========================================== #
+
+#' Forecast using caret based Machine learning techniques
+#'
+#' @param train Takes a TS object as an argument
+#' @param test
+#' @param
+#' @param
+#' @param
+#' @param
+#'
+#' @example
+
+#' @export
+
+
+
+
+###################################
+# Caret
+####################################
+
+
+caret_forecast <- function (){
+
+}
