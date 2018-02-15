2a7c2a5729e89d9248463eeeadaa5061de3c4222
JaredChung <jared_chung@hotmail.com>
2018-02-15 17:39:35 +1100

    shifted forecasting code to another R script

--- R/auto_forecast.R
+++ R/auto_forecast.R
@@ -15,27 +15,9 @@
 #' @export
 
 
-# Access forecast package
-# run_forecast <- function(train, test,FUN, name, timeslice,  train_regressor= NULL, test_regressor=NULL,...) {
-#
-#   # check if there is external regressors
-#   if(is.null(train_regressor)) {
-#     model <- FUN(train, ...)
-#     predictions <-forecast::forecast(model, h = length(test))
-#   } else {
-#     model <- FUN(train, xreg = train_regressor , ...)
-#     predictions <-forecast::forecast(model, h = length(test), xreg = test_regressor)
-#   }
-#
-#   result <- forecast::accuracy(predictions, test) %>%
-#                                   as.data.frame() %>%
-#                                   tibble::rownames_to_column() %>%
-#                                   dplyr::mutate(model = name,
-#                                          timeslice = timeslice)
-#
-#   return(list(predictions = as.data.frame(predictions), result = result, model = model ))
-# }
-
+#######################################
+# Main Forecast Function
+#######################################
 
 automatic_forecast <- function(data,
                                cv_horizon = 1,
@@ -82,42 +64,6 @@ automatic_forecast <- function(data,
       print(sprintf("--------- Train Length %s", length(trainslices[[i]])))
       print(sprintf("--------- Test Length %s",length(testslices[[i]])))
     }
-
-    # ets <- run_forecast(train = data[trainslices[[i]]],
-    #                     test = data[testslices[[i]]],
-    #                     FUN = forecast::ets,
-    #                     name = 'ets',
-    #                     timeslice = i,
-    #                     lambda = forecast::BoxCox.lambda(data[trainslices[[i]]]))
-    #
-    # arima <- run_forecast(train = data[trainslices[[i]]],
-    #                     test = data[testslices[[i]]],
-    #                     FUN = forecast::auto.arima,
-    #                     name = 'arima',
-    #                     timeslice = i,
-    #                     lambda = forecast::BoxCox.lambda(data[trainslices[[i]]]))
-    #
-    # tbats <- run_forecast(train = data[trainslices[[i]]],
-    #                     test = data[testslices[[i]]],
-    #                     FUN = forecast::tbats,
-    #                     name = 'tbats',
-    #                     timeslice = i)
-    #
-    # nnetar <- run_forecast(train = data[trainslices[[i]]],
-    #                     test = data[testslices[[i]]],
-    #                     FUN = forecast::nnetar,
-    #                     name = 'nnetar',
-    #                     timeslice = i,
-    #                     lambda = forecast::BoxCox.lambda(data[trainslices[[i]]]),
-    #                     train_regressor = trainslices_xreg,
-    #                     test_regressor = testslices_xreg)
-    #
-    # thetaf <- run_forecast(train = data[trainslices[[i]]],
-    #                     test = data[testslices[[i]]],
-    #                     FUN = forecast::thetaf,
-    #                     name = 'thetaf',
-    #                     timeslice = i)
-
 
     standard_forecast_result <- standard_forecast(train = data[trainslices[[i]]],
                                                   test = data[testslices[[i]]],
