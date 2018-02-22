9fc7e0087cbcbd045d506c2896501c4f3c780a9b
JaredChung <jared_chung@hotmail.com>
2018-02-22 16:35:20 +1100

    add glmnet

--- R/caret_forecast.R
+++ R/caret_forecast.R
@@ -26,6 +26,59 @@
 ####################################
 
 
-caret_forecast <- function (){
+caret_forecast <- function (train,
+                            test){
+
+
+
+      # Elastic net
+      lambda_grid <- 10 ^ seq(2, -2, length = 100)
+      alpha_grid <- seq(0, 1, length = 10)
+
+      glmnet_grid <- expand.grid(lambda = lambda_grid,
+                                 alpha = alpha_grid)
+
+      glmnet_model <- train(value ~ .,
+                            data = train,
+                            method = "glmnet",
+                            tuneGrid = glmnet_grid,
+                            trControl = )
+
+
+      # Random Forest
+
+
+      # Gradient Boosted Machine
+
+
+
+
+
 
 }
+
+
+
+
+
+
+library(fpp2)
+
+data <- a10
+
+trainslices <- cross_validation_data(data)$train
+testslices <- cross_validation_data(data)$test
+
+
+fit <- auto.arima(data[trainslices[[1]]])
+
+predict <- forecast(fit,h = 1)
+
+error_metric(data[testslices[[1]]],as.numeric(predict$mean))
+
+forecast::accuracy(predict,data[testslices[[1]]])
+
+
+
+
+
